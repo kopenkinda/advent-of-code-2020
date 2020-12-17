088354ab08511d5a29cfcf67e8e4e68040fe3b01
kopenkinda <kopenkin.da@gmail.com>
2020-12-18 02:26:14 +0500

    day2#part1

---
+++ day02.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+
+const data = fs.readFileSync('./day02_input.txt').toString().split('\n');
+
+function checkValid(rawData) {
+  const [rawPolicy, letterRaw, password] = rawData.split(' ');
+  const policy = rawPolicy.split('-').map((x) => +x);
+  const letter = letterRaw[0];
+  const letters = password.split('').reduce((acc, v) => {
+    if (acc[v]) {
+      acc[v] += 1;
+    } else {
+      acc[v] = 1;
+    }
+    return acc;
+  }, {});
+  if (letters[letter] >= +policy[0] && letters[letter] <= +policy[1]) return true;
+  return false;
+}
+
+let valid = 0;
+
+data.forEach((piece) => {
+  if (checkValid(piece)) {
+    valid += 1;
+  }
+});
+console.log(valid);
