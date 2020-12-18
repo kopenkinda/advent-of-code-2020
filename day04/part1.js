3d9cde21016a9ebd27b4472ad1d376f0b376ff93
kopenkinda <kopenkin.da@gmail.com>
2020-12-18 13:37:40 +0500

    Day4#Part1

---
+++ day04/part1.js
@@ -0,0 +1,35 @@
+const { readFileSync } = require('fs');
+
+const data = readFileSync('./input.txt').toString().split('\n\n');
+
+function verifyPassport(rawPassport) {
+  const neededFields = ['byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid', 'cid'];
+  let presentFields = 0;
+  let hasCID = false;
+  const passport = rawPassport.split('\n').join(' ').split(' ').map((x) => x.split(':'));
+  passport.forEach(([field, value]) => {
+    if (neededFields.includes(field)) {
+      if (field === 'cid') {
+        hasCID = true;
+      }
+      presentFields += 1;
+    }
+  });
+  if (
+    (presentFields === neededFields.length)
+    || (presentFields === neededFields.length - 1 && !hasCID)
+  ) {
+    return true;
+  }
+  return false;
+}
+
+let verified = 0;
+
+data.forEach((passport) => {
+  if (verifyPassport(passport)) {
+    verified += 1;
+  }
+});
+
+console.log(verified);
