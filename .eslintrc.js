51d252859e376599380538cd548716963bbdc1a4
kopenkinda <kopenkin.da@gmail.com>
2020-12-22 21:18:13 +0500

    Added typescript

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,11 +4,16 @@ module.exports = {
     es2021: true,
     node: true,
   },
+  parser: '@typescript-eslint/parser',
   extends: [
     'airbnb-base',
   ],
+  plugins: [
+    '@typescript-eslint',
+  ],
   parserOptions: {
     ecmaVersion: 12,
+    sourceType: 'module',
   },
   rules: {
     'no-console': 0,
